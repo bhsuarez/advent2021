be92cf848000cf8af0b41387655b8bbcf057c531
Brian H Suarez <38057048+bhsuarez@users.noreply.github.com>
2021-12-03 17:08:54 -0600

    dict stuff

--- 1.py
+++ 1.py
@@ -4,9 +4,7 @@ if __name__ == '__main__':
     measurements = loader.load_advent_input_as_dict('1.txt')
     increase_count = 0
 
-    for measurement in measurements.values():
-        if measurement[0] > measurement[3]:
-            print(str(measurement) + str(" no previous measurement"))
-            last_measurement = measurement
+    if measurements[2] > measurements[111]:
+        print("I don't know")
 
     print("Number of increases: "+str(increase_count))
