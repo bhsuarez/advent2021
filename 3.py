c46271ce6a5a92b500ce1339ab86ac5e0988786e
Brian H Suarez <38057048+bhsuarez@users.noreply.github.com>
2021-12-03 19:13:44 -0600

    Update 3.py

--- 3.py
+++ 3.py
@@ -5,12 +5,7 @@ if __name__ == '__main__':
     report = loader.load_advent_input_as_dict("3.txt")
     count = 0
     gamma = ""
-    first,second,third,fourth,fifth=0,0,0,0,0
     while count < len(report):
-        print(report[count])
-        first += int(report[count][0])
-        second += int(report[count][1])
-        third += int(report[count][2])
-        fourth += int(report[count][3])
-        fifth += int(report[count][4])
+        for digit in len(report[count]):
+            print(report[digit])
         count +=1
